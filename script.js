284796e45ec9e1228a091b05160dff621033b052
kiran.hirani <kiran.hirani@operative.com>
2023-05-22 10:56:04 +0530

    Picture in Picture

---
+++ script.js
@@ -0,0 +1,24 @@
+const videoElement = document.getElementById("video"),
+  button = document.getElementById("button");
+
+//Prompt the user a media stream, pass that to our video element then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    //Catch errors
+  }
+}
+
+button.addEventListener("click", async () => {
+  button.disabled = true;
+  await videoElement.requestPictureInPicture();
+  button.disabled = false;
+});
+
+//On Load
+selectMediaStream();
